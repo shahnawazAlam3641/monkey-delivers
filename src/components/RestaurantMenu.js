17b3f4190b41b7fb08fed943bb3d059e18d77bc5
Shahnawaz Alam <shahnwazalam3641@gmail.com>
2024-03-23 03:35:26 +0530

    code menu items

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -30,10 +30,96 @@ const RestaurantMenu = () => {
   return (
     menu?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map((menuType, index)=>{
       return index>0 ? (
-        <h1>{menuType?.card?.card?.title}</h1>
+
+        <div>
+        <h1 className='bg-blue-300'>{menuType?.card?.card?.title}</h1>
+        
+        {menuType?.card?.card?.itemCards ?  menuType?.card?.card?.itemCards.map((item)=>{
+
+          
+return (
+<div>
+  <div>
+  <p>{item?.card?.info?.name}</p>
+  <p>₹{item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}</p>
+  <p>{item?.card?.info?.description}</p>
+</div>
+<div>
+  <img/>
+  <button>add</button>
+  <div>
+    <button>-</button>
+    <p>0</p>
+    <button>+</button>
+  </div>
+</div>
+</div>
+)
+
+} ) 
+: menuType?.card?.card?.carousel ? <h1 className='bg-red-500'>carousel</h1> 
+: menuType?.card?.card?.categories ? menuType?.card?.card?.categories.map((categoryItem)=>{
+    return <h1 className='bg-yellow-300'>{categoryItem?.title}</h1>
+    
+
+})
+: null}
+        </div>
+
       ) : null
     })
   )
 }
+
+// const RestaurantMenu = () => {
+//   const [menu, setMenu] = useState(null);
+//   const { resId } = useParams();
+
+//   useEffect(() => {
+//     getMenu();
+//   }, []);
+
+//   const getMenu = async () => {
+//     const response = await fetch('https://thingproxy.freeboard.io/fetch/' + RESTAURANT_MENU_API + resId);
+//     const menuData = await response.json();
+//     setMenu(menuData);
+//     console.log(menuData);
+//   };
+
+//   if (!menu) return <h1>Loading Menu</h1>;
+
+//   return (
+//     <div>
+//       {menu?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map((menuType, index) => {
+//         return index > 0 ? (
+//           <div key={index}>
+//             <h1>{menuType?.card?.card?.title}</h1>
+//             {menuType?.card?.card?.itemCards ? (
+//               menuType?.card?.card?.itemCards.map((item, itemIndex) => (
+//                 <div key={itemIndex}>
+//                   <div>
+//                     <p>name</p>
+//                     <p>price</p>
+//                     <p>desc</p>
+//                   </div>
+//                   <div>
+//                     <img  />
+//                     <button>Add</button>
+//                     <div>
+//                       <button>-</button>
+//                       <p>0</p>
+//                       <button>+</button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               ))
+//             ) : null}
+//           </div>
+//         ) : null;
+//       })}
+//     </div>
+//   );
+// };
+
 
 export default RestaurantMenu
