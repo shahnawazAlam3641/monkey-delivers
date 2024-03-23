a36089b4c5157e6cbe308cba2a12ec785a25e00d
Shahnawaz Alam <shahnwazalam3641@gmail.com>
2024-03-24 03:27:30 +0530

    menu item dropdown pending

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,9 @@
  export const MAIN_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export const RESTAURANT_MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId="
+
+export const SEARCH_DISH_API = "https://www.swiggy.com/dapi/restaurants/search/v3?lat=28.7040592&lng=77.10249019999999&submitAction=SUGGESTION&str="
+
  export const CUISINE_IMG_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/"
 
  export const claudinaryImgCDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
