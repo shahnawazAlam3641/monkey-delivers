5befc683da774c7f82db145021bac80aaf5a6688
Shahnawaz Alam <shahnwazalam3641@gmail.com>
2024-04-12 17:17:47 +0530

    fix header path in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/HEader";
+import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Search from "./components/Search";
