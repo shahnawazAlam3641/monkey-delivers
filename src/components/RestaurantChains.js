1fa60af7baed5d31708a2bd6ad65b6af0ab18578
Shahnawaz Alam <shahnwazalam3641@gmail.com>
2024-04-12 17:25:05 +0530

    fix RestaurantCard path in RestaurantChains

--- src/components/RestaurantChains.js
+++ src/components/RestaurantChains.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import RestaurantCard, { DiscountedRestaurant } from "./restaurantCard";
+import RestaurantCard, { DiscountedRestaurant } from "./RestaurantCard";
 import { Link } from "react-router-dom";
 import { claudinaryImgCDN } from "../utils/constants";
 
