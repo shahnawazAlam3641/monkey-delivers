7e522e6a090483672973c2917828509812027a71
shahnawazAlam3641 <shahnawazalam3641@gmail.com>
2024-12-31 01:12:37 +0530

    reduce footer text sixe

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-between bg-[#171a29] text-white p-4 mt-5 ">
-      <p>Built with 🧡 by Shahnawaz Alam</p>
+      <p className="text-sm">Built with 🧡 by Shahnawaz Alam</p>
       <div className="flex gap-5 ">
         <a
-          className="hover:underline  transition-all duration-200"
+          className="hover:underline  transition-all duration-200 text-sm"
           href="https://github.com/shahnawazAlam3641/monkey-delivers"
           target="_blank"
         >
           Github
         </a>
         <a
-          className="hover:underline  transition-all duration-200"
+          className="hover:underline  transition-all duration-200 text-sm"
           href="https://www.linkedin.com/in/alam-shahnawaz/"
           target="_blank"
         >
